977387ebdf71272643186cec86f779f21e2b187e
jkoller90 <jmk4411@gmail.com>
2017-10-15 14:52:57 -0400

    receive.js has code for receiving sms

--- receiveMSG.js
+++ receiveMSG.js
@@ -1,3 +1,5 @@
+/*
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -19,8 +21,31 @@ app.post('/message',function(req,res){
     </Response>
 
 `
+             //TWIML
     )
 })
 
 app.listen(3000 || process.env.PORT) ;
     
+*/
+
+var http = require('http');
+var express = require('express');
+var twilio = require('twilio');
+
+var app = express();
+
+app.post('/sms/', function(req,res){
+    var twilio = require('twilio');
+    var twiml = new twilio.TwimlResponse();
+    twiml.message("OUTBOUT MESSAGE");
+    res.writeHead(200,{'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3000 || process.env.PORT, function(){
+    console.log('Listening on 3000');
+})
+
+//ngrok http 3000
+//get url to A Message Comes In w/ /sms to end of url
