6003abc9cbf5f7fe633384a95559d85ecc0c1c4e
jkoller90 <jmk4411@gmail.com>
2017-10-17 19:43:56 -0400

    now writing boolean cases to handle various inputs. also added boolean flags to regulate how the post request should affect the dialogue

--- sms-service.js
+++ sms-service.js
@@ -5,73 +5,114 @@ var MessagingResponse = require('twilio').twiml.MessagingResponse;
 var bodyParser = require('body-parser');
 
 var client = require('twilio')(
-	'ACc694cec59a35c6b5830571760dc626a6',
-	'af0ddb5adb3d8d38d04babd5b03b24db'
+    'ACc694cec59a35c6b5830571760dc626a6',
+    'af0ddb5adb3d8d38d04babd5b03b24db'
 );
 
-setInterval(function () {
-	for(var i = 0 ; i < users.length; i++){
-			client.messages.create({
-		from: "+19149966800",
-		to: "+19143301533",
-		body: "Hello from Jonathan Koller CS 643 Fall 2017!"
-	}, function (err, message) {
-		if (err) console.error(err.message);
-	});
-		console.log("Send daily info message to user: "+users[i].name+" on phone number: "+users[i].number);
-		console.log("Daily info message: "+dailyInfo)
-	}
 
-	//every 400 seconds
+//to be put into the timer on line 73 or so
+setInterval(function () {
+    for (var i = 0; i < users.length; i++) {
+        console.log(users[i]['number']);
+        client.messages.create({
+            from: "+19149966800",
+            to: users[i]['number'],
+            body: 'Hello! Here\'s today\'s tip: \n' + questions[globalDay]['info'] + ' \n Would you like to test your knowledge? Respond with "Yes" to play!'
+        }, function (err, message) {
+            if (err) console.error(err.message);
+        });
+    }
+
+    //every 400 seconds
 }, 4000);
 
 var app = express();
 
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 
 app.post('/sms', function (req, res) {
-	var twilio = require('twilio');
-	var twiml = new MessagingResponse();
+    var twilio = require('twilio');
+    var twiml = new MessagingResponse();
+    var isQuestionOne = false; //this is a flag that gets switched when the quiz is initialized
+    var isQuestionTwo = false;
+    var isQuestionThree = false;
+    var msgBody = req.body.Body;
 
-	var msgBody = req.body.Body;
-	console.log(msgBody == 'hey');
-		if(msgBody == 'yes' || msgBody == 'start'){
-			
-		}
+    if (!isQuestionOne && !isQuestionTwo && !isQuestionThree) {
+        if (msgBody.trim().toLowerCase() == 'si' || msgBody.trim().toLowerCase() == 'yes' || msgBody.trim().toLowerCase() == 'start') {
+            
+        }
+    }
 
-	twiml.message('The Robots are coming! Head for the hills!');
-	res.writeHead(200, {
-		'Content-Type': 'text/xml'
-	});
-	res.end(twiml.toString());
+
+    res.writeHead(200, {
+        'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
 });
 
 http.createServer(app).listen(3000, function () {
-	console.log("Express server listening on port 3000");
+    console.log("Express server listening on port 3000");
 });
 
 
 
-function Send_DailyMessagePrompt() {
-	/* 
-	new message every day
-	listens for keyword to start quiz
-	calls to DB for quiz info
-	*/
+function queryQuiz(userAnswerAttempt) {
+    //does lookup and returns boolean
+    if () return false;
+    else return true;
 }
 
+var dayInMilliseconds = 1000 * 60 * 60 * 24;
+var globalDay = 0;
 
-// extra syntax for messages just in case:
 
-//	res.send(`
-//	<Response>
-//		<Message>
-//				Hello ${msgFrom}. You said: ${msgBody}
-//		</Message>
-//	</Response>
-//`);
 
-//	var msgFrom = req.body.From;
-//	console.log(msgFrom);
+setInterval(function () {
+    var lenInfoSet = questions.length;
+    if (globalDay == lenInfoSet - 1) globalDay = 0;
+    else globalDay++;
+}, dayInMilliseconds);
+
+var questions = [
+    {
+        "questionId": 0,
+        "questionName": "Test question Name",
+        "info": "Basic information",
+        "question": "What is blah blah, basic question?",
+        "answer": "Answer to question"
+    },
+    {
+        "questionId": 1,
+        "questionName": "Test question Name1",
+        "info": "Basic information1",
+        "question": "What is blah blah, basic question1?",
+        "answer": "Answer to question1"
+    },
+    {
+        "questionId": 2,
+        "questionName": "Test question Name2",
+        "info": "Basic information2",
+        "question": "What is blah blah, basic question2?",
+        "answer": "Answer to question2"
+    },
+]
+
+var users = [
+    {
+        'name': 'Jon Koller',
+        'number': '+19143301533'
+    },
+
+    {
+        "name": "Bruce Wayne",
+        "number": "+19174445555"
+    }
+	,
+    {
+        "name": "Arnold Schwartzenager",
+        "number": "+12123334444"
+    }
+	]
--- sms.js
+++ sms.js
@@ -1,29 +1,41 @@
-var questions =[
- {"questionId" : 0,
-  "questionName" : "Test question Name",
-  "info" : "Basic information",
-  "question" : "What is blah blah, basic question?",
-	"answer" : "Answer to question"},
-	{"questionId" : 1,
-	 "questionName" : "Test question Name1",
-	 "info" : "Basic information1",
-	 "question" : "What is blah blah, basic question1?",
- 	 "answer" : "Answer to question1"},
-	 {"questionId" : 2,
- 	 "questionName" : "Test question Name2",
- 	 "info" : "Basic information2",
- 	 "question" : "What is blah blah, basic question2?",
-  	 "answer" : "Answer to question2"},
+var questions = [
+    {
+        "questionId": 0,
+        "questionName": "Test question Name",
+        "info": "Basic information",
+        "question": "What is blah blah, basic question?",
+        "choices":[1,2,3],
+        "answer": 0
+    },
+    {
+        "questionId": 1,
+        "questionName": "Test question Name1",
+        "info": "Basic information1",
+        "question": "What is blah blah, basic question1?",
+        "choices":[1,2,3],
+        "answer": 1
+    },
+    {
+        "questionId": 2,
+        "questionName": "Test question Name2",
+        "info": "Basic information2",
+        "question": "What is blah blah, basic question2?",
+        "choices":[1,2,3],
+        "answer": 2
+    },
 ]
 var users = [
-	{'name':'Jon Koller',
-	'number':'+19143301533'},
-	
-//		{"name":"Bruce Wayne",
-//		"number": "+19174445555"}
-//	,
-//		{"name":"Arnold Schwartzenager",
-//		"number" : "+12123334444"}
+    {
+        'name': 'Jon Koller',
+        'number': '+19143301533'
+    },
+    {
+        "name": "Bruce Wayne",
+        "number": "+19174445555"
+		}, {
+        "name": "Arnold Schwartzenager",
+        "number": "+12123334444"
+		}
 	]
 
 var globalDay = 0;
@@ -35,105 +47,108 @@ var util = require('util');
 var port = process.env.PORT || 3000;
 
 var server = http.createServer(function (req, res) {
-	// console.log(questions.questionId)
-	// console.log(process.env);
-	startApp();
-	if (req.method.toLowerCase() == 'get') {
-		displayForm(res);
-	} else if (req.method.toLowerCase() == 'post') {
-		formSubmission(req, res);
-	}
+    // console.log(questions.questionId)
+    // console.log(process.env);
+    startApp();
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        formSubmission(req, res);
+    }
 });
 
 function displayForm(res) {
-	fs.readFile('index.html', function (err, data) {
-		res.writeHead(200, {
-			'Content-Type': 'text/html',
-			'Content-Length': data.length
-		});
-		res.write(data);
-		res.end();
-	});
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
 };
 
 
 function formSubmission(req, res) {
-	var fields = [];
-	var values = [];
-	var form = new formidable.IncomingForm();
-	form.on('field', function (field, value) {
-		fields[field] = value;
-		values.push(value);
-	});
+    var fields = [];
+    var values = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+        fields[field] = value;
+        values.push(value);
+    });
 
-	form.on('end', function () {
-		res.writeHead(200, {
-			'content-type': 'text/plain'
-		});
-		res.end(util.inspect({
-			fields: fields
-		}));
+    form.on('end', function () {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.end(util.inspect({
+            fields: fields
+        }));
 
-	});
-	form.parse(req);
-}
-function sendText(from, to, message){
-	client = require('twilio')(
-		values[1], values[2]
-	);
-			client.messages.create({
-					from: from,
-					to: to,
-					body: message
-			}, function (err, message) {
-					if (err) console.error(err.message);
-			});
+    });
+    form.parse(req);
 }
 
-function startApp(){
-	// Change time interval to daily when you are done testing
-	setInterval(function(){ dailyTasks(globalDay) },2000);
+function sendText(from, to, message) {
+    client = require('twilio')(
+        values[1], values[2]
+    );
+    client.messages.create({
+        from: from,
+        to: to,
+        body: message
+    }, function (err, message) {
+        if (err) console.error(err.message);
+    });
 }
 
-function dailyTasks(day){
-	//Manages globalDay variable, to keep looping through questions array
-	//I put this first so we can use the globalDay variable to look up other info
-	//pertaining to that day, like quiz Questions/Answers
-	var lenInfoSet = questions.length;
-	if (day == lenInfoSet-1){
-		globalDay = 0;
-	} else {
-		globalDay = day+1
-	}
+function startApp() {
+    // Change time interval to daily when you are done testing
+    setInterval(function () {
+        dailyTasks(globalDay)
+    }, 2000);
+}
 
-	//Daily Logic
-	var dailyInfo = questions[day].info
-	var arrayLength = users.length;
+function dailyTasks() {
+    //Manages globalDay variable, to keep looping through questions array
+    //I put this first so we can use the globalDay variable to look up other info
+    //pertaining to that day, like quiz Questions/Answers
+    var lenInfoSet = questions.length;
+    if (globalDay == lenInfoSet - 1) {
+        globalDay = 0;
+    } else {
+        globalDay = day + 1
+    }
 
-	for (var i = 0; i < arrayLength; i++) {
-    console.log("Send daily info message to user: "+users[i].name+" on phone number: "+users[i].number);
-		console.log("Daily info message: "+dailyInfo)
-	}
+    //Daily Logic
+    var dailyInfo = questions[day].info
+    var arrayLength = users.length;
+
+    for (var i = 0; i < arrayLength; i++) {
+        console.log("Send daily info message to user: " + users[i].name + " on phone number: " + users[i].number);
+        console.log("Daily info message: " + dailyInfo)
+    }
 
 
 }
 
-function sendTodaysQuiz(day,number){
-	//We need to get the phone number from the listener, not sure how this works
-	var dailyQuizQuestion = questions[day].question
-	console.log(dailyQuizQuestion)
+function sendTodaysQuiz(day, number) {
+    //We need to get the phone number from the listener, not sure how this works
+    var dailyQuizQuestion = questions[day].question
+    console.log(dailyQuizQuestion)
 }
 
-function checkAnswer(day, answer){
-	//Answer should also come from the listener, I think we should have a seperate
-	//function like this for each action that can happen, and one response function
-	// that will call these based on the keyword sent by the user
-	var correctAns = questions[day].answer
-	if(correctAns == answer){
-		console.log("Send congrats text saying the answer was right (tally score?)")
-	} else{
-		console.log("Send text that the answer was wrong and right answer: "+correctAns)
-	}
+function checkAnswer(day, answer) {
+    //Answer should also come from the listener, I think we should have a seperate
+    //function like this for each action that can happen, and one response function
+    // that will call these based on the keyword sent by the user
+    var correctAns = questions[day].answer
+    if (correctAns == answer) {
+        console.log("Send congrats text saying the answer was right (tally score?)")
+    } else {
+        console.log("Send text that the answer was wrong and right answer: " + correctAns)
+    }
 }
 
 server.listen(port);
