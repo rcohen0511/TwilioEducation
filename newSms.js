5abdfc98f9e79b9d1f8b2293bb575d7367682404
jkoller90 <jmk4411@gmail.com>
2017-10-25 16:54:37 -0400

    change to version 3

--- newSms.js
+++ newSms.js
@@ -1,17 +1,18 @@
-var http = require('http');
-var express = require('express');
-var twilio = require('twilio');
+onst http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-var app = express();
+const app = express();
 
-app.post('/sms', function(req, res) {
-  var twilio = require('twilio');
-  var twiml = new twilio.TwimlResponse();
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
   twiml.message('The Robots are coming! Head for the hills!');
+
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, function () {
-  console.log("Express server listening on port 1337");
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
 });
