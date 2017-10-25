e8807e607eb4323a3fe3e3562876a477541e6555
jkoller90 <jmk4411@gmail.com>
2017-10-25 16:58:23 -0400

    post replaced with get

--- newSms.js
+++ newSms.js
@@ -4,7 +4,16 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
+//app.post('/sms', (req, res) => {
+//  const twiml = new MessagingResponse();
+//
+//  twiml.message('The Robots are coming! Head for the hills!');
+//
+//  res.writeHead(200, {'Content-Type': 'text/xml'});
+//  res.end(twiml.toString());
+//});
+
+app.get('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
