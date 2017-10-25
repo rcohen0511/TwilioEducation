f36992e5648d685da8b7e523f5a86f73581c02ed
Robert Cohen <rcohen0511@gmail.com>
2017-10-25 15:04:11 -0400

    reset

--- README.md
+++ README.md
@@ -15,3 +15,32 @@ The same goes for these at the top of sms.js:
 	process.env.TWILIO_AUTH_TOKEN
 	
 You should be able to hard-code the info in the outermost scope of sms.js, though, and be good to go. 
+
+
+# Needed Fixes
+1. Phone number input is hard-coded to 3, so successive server calls is not possible
+2. This is not deployable on Heroku for some reason - need to do some research on Web Hooks
+3. Many other code nightmares await because it's a hack 'n slash job of a script...
+4. Stream of data is not linear - because the forms are not in the same order as the original process...
+
+{ fields: 
+   [ Tip: 'A is A',
+     Answer: 'Yes',
+     choice1: 'No',
+     choice2: 'Maybe',
+     Number: '' ] 
+}
+
+Should be
+
+{fields:
+    [Tip: 'A is A',
+    Question: 'Is A A?',
+    Answer: 'Yes',
+    choice1: 'No',
+    choice2: 'Maybe',
+    
+    ...could be an error due to the fact that we are adding numbers through the form and it's awkwardly hardcoded to max out at 3 input per server session
+
+    ]
+}
---
+++ sms-service.js
@@ -0,0 +1,199 @@
+var http = require('http');
+
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+var port = process.env.PORT || 3000;
+
+var express = require('express');
+var twilio = require('twilio');
+var MessagingResponse = require('twilio').twiml.MessagingResponse;
+var bodyParser = require('body-parser');
+
+var questions = [
+    {
+        "questionId": 0,
+        "questionName": "Test question Name",
+        "info": "Do you wash your hands before you eat? You should! Otherwise you lend yourself vulnerable to nasty bacteria found on the street.",
+        "question": "How many minutes should you wash your hands?",
+        "choices": ['1. 2 minutes', '2. 1 minute', '3. 4 minutes'],
+        "answer": 1
+    },
+    {
+        "questionId": 1,
+        "questionName": "Test question Name1",
+        "info": "Basic information1",
+        "question": "What is blah blah, basic question1?",
+        "choices": [1, 2, 3],
+        "answer": 2
+    },
+    {
+        "questionId": 2,
+        "questionName": "Test question Name2",
+        "info": "Basic information2",
+        "question": "What is blah blah, basic question2?",
+        "choices": [1, 2, 3],
+        "answer": 3
+    },
+]
+var users = [
+    {
+        'name': 'Jon Koller',
+        'number': '+19143301533'
+    },
+
+    {
+        "name": "Bruce Wayne",
+        "number": "+19174445555"
+    }
+	,
+    {
+        "name": "Arnold Schwartzenager",
+        "number": "+12123334444"
+    }
+	]
+
+var client = require('twilio')(
+    'ACc694cec59a35c6b5830571760dc626a6',
+    'af0ddb5adb3d8d38d04babd5b03b24db'
+);
+
+function displayForm(res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+};
+
+var values = [];
+
+function formSubmission(req, res) {
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+        fields[field] = value;
+        values.push(value);
+    });
+
+    form.on('end', function () {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.end(util.inspect({
+            fields: fields
+        }));
+
+        questions[0] = {
+            "questionId": 0,
+            "questionName": "Test question Name",
+            "info": values[0],
+            "question": values[1],
+            "choices": ['1.' + values[1], '2.' + values[2], '3.' + values[3]],
+            "answer": 1
+        }
+        console.log(questions);
+    });
+    form.parse(req);
+}
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+var formSubmitted = false;
+var acceptedQuiz = false;
+var isQuestionOne = false; //this is a flag that gets switched when the quiz is initialized
+
+app.get('/', function (req, res) {
+    displayForm(res);
+});
+
+app.post('/', function (req, res) {
+    formSubmission(req, res);
+    formSubmitted = true;
+
+    //setInterval
+    setTimeout(function () {
+        for (var i = 0; i < users.length; i++) {
+            client.messages.create({
+                from: "+19149966800",
+                //                to: users[i]['number'],
+                to: values[i + 4],
+                body: 'Hello! Here\'s today\'s tip: \n' + questions[globalDay]['info'] + ' \n Would you like to test your knowledge? Respond with "Yes" to answer a quiz question!'
+            }, function (err, message) {
+                if (err) console.error(err.message);
+            });
+        }
+    }, 1000);
+    //dayInMilliseconds		
+})
+
+app.post('/sms', function (req, res) {
+    var twilio = require('twilio');
+    var twiml = new MessagingResponse();
+
+    var msgBody = req.body.Body;
+
+//experimental: registration logical scheme
+    /* 
+    if(msgBody.trim().toLowerCase() == 'join'){
+        console.log(req.body);
+    }
+    */
+    
+    if (!acceptedQuiz) { //if hasnt accepted the quiz attempt
+        if (msgBody.trim().toLowerCase() == 'yes') {
+            acceptedQuiz = true;
+            isQuestionOne = true;
+
+
+            var messageString = 'Here is your question! \nIs A A?\n\nChoices:\n';
+/* yikes  - needs to reference a backend to query for all numbers in data base*/
+            for (var i = 0; i < 3; i++) {
+                messageString += '\n' + questions[globalDay]['choices'][i];
+            }
+            twiml.message(messageString);
+        } else if (msgBody.trim().toLowerCase() == 'no') {
+            twiml.message('Fair enough!');
+        } else {
+            twiml.message('Please check your spelling or re-type \'Yes\' to take the quiz!');
+        }
+    } else if (acceptedQuiz && isQuestionOne) {
+        if ((globalDay == 0 && msgBody == 1) || (globalDay == 1 && msgBody == 1) || (globalDay == 2 && msgBody == 3)) {
+            isQuestionOne = false;
+            twiml.message('Correct! Great job! Stay tuned tomorrow for more!');
+        } else if (parseInt(msgBody) > 3 || parseInt(msgBody) < 1 || isNaN(msgBody)) {
+            twiml.message('Please check your response! Choose 1, 2, or 3!');
+        } else {
+            isQuestionOne = false;
+            twiml.message('I\'m sorry, but that\'s incorrect. Try again tomorrow!');
+        }
+    }
+    
+    res.writeHead(200, {
+        'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3000, function () {
+    console.log("Express server listening on port 3000");
+});
+
+var dayInMilliseconds = 1000 * 60 * 60 * 24;
+var globalDay = 0;
+
+
+/*
+setInterval(function () {
+	var lenInfoSet = questions.length;
+	if (globalDay == lenInfoSet) globalDay = 1;
+	else globalDay++;
+}, dayInMilliseconds);
+*/
